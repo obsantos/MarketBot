aeca5af8df6e7cad4a26589dcc6c3689af1497ee
obsantos <omar.busto@gmail.com>
2021-03-25 16:56:35 -0400

    Updated stock.py to show pre/post market data

--- stock.py
+++ stock.py
@@ -9,34 +9,60 @@
 import sys, argparse
 import requests
 
-YAHOO_FINANCE_URL = "https://query1.finance.yahoo.com/v7/finance/quote?formatted=true&symbols={0}&fields=regularMarketChangePercent%2CregularMarketChange%2CregularMarketPrice%2ClongName%2CshortName"
+YAHOO_FINANCE_URL = "https://query1.finance.yahoo.com/v7/finance/quote?formatted=true&symbols={0}&fields={1}"
+FIELDS = "regularMarketChangePercent%2CregularMarketChange%2CregularMarketPrice%2ClongName%2CshortName%2CmarketState%2CpostMarketChangePercent%2CpostMarketChange%2CpostMarketPrice%2CpreMarketChange%2CpreMarketPrice%2CpreMarketChangePercent"
+MARKET_STATE_REG = "REGULAR"
+MARKET_STATE_PRE = "PRE"
+MARKET_STATE_POST = "POST"
+
+def get_price_for_market_state(state, result):
+    """Returns the price for the current state of the market"""
+    if state == MARKET_STATE_PRE:
+        return {
+            "current": result['preMarketPrice']['fmt'],
+            "previous": result['regularMarketPrice']['fmt'],
+            "change": result['preMarketChange']['fmt'],
+            "percent": result['preMarketChangePercent']['fmt']
+        }
+    elif state == MARKET_STATE_POST:
+        return {
+            "current": result['postMarketPrice']['fmt'],
+            "previous": result['regularMarketPrice']['fmt'],
+            "change": result['postMarketChange']['fmt'],
+            "percent": result['postMarketChangePercent']['fmt']
+        }
+    else:
+        return {
+            "current": result['regularMarketPrice']['fmt'],
+            "previous": result['regularMarketPreviousClose']['fmt'],
+            "change": result['regularMarketChange']['fmt'],
+            "percent": result['regularMarketChangePercent']['fmt']
+        }        
+        
 
 def query_symbol_details(symbol):
     """Given a stock symbol query Yahoo Finance for details and returns a simplified object"""
-    response = requests.get(YAHOO_FINANCE_URL.format(symbol)).json()
+    response = requests.get(YAHOO_FINANCE_URL.format(symbol, FIELDS)).json()
+    print(response)
     try:
         result = response['quoteResponse']['result'][0]
         name = result['longName']
-        previous = result['regularMarketPreviousClose']['fmt']
-        current = result['regularMarketPrice']['fmt']
-        change_percent = result['regularMarketChangePercent']['fmt']
-        change = result['regularMarketChange']['fmt']
+        state = result['marketState']
+        price = get_price_for_market_state(state, result)
         return {
             "symbol": symbol,
             "name": name,
-            "previous": previous,
-            "current": current,
-            "change": change,
-            "percent": change_percent
+            "state": state,
+            "price": price
         }
     except ValueError:
         return {}
 
 def print_symbol_details(details):
     """Prints the symbol\'s details"""
-    print(details['name'] + " (" + details['symbol'] + ")")
-    print("Price: " + details['current'])
-    print("Previous: " + details['previous'] + " - Change: " + details['change'] + " (" + details['percent'] + ")")
+    print(details['name'] + " (" + details['symbol'] + ") - " + details['state'])
+    print("Price: " + details['price']['current'])
+    print("Previous: " + details['price']['previous'] + " - Change: " + details['price']['change'] + " (" + details['price']['percent'] + ")")
 
 def main():
     parser = argparse.ArgumentParser(description='Script that queries Yahoo Finance to retrieve a symbol\'s price')
